cfbbd8a5a20c92a8c16925c067c5c40be6474a38
BasankoSV <basankosv@gmail.com>
2019-12-19 12:04:17 +0500

    Написал две функции: getMinMax(str) и showSalary(data, age)

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -4,5 +4,13 @@
  * @param {number} age - максимальный возраст
  * @returns {string}
  */
+
 function showSalary(data, age) {
+  let result = '';
+  let someUser = data.filter(item => item.age <= age);
+  for (let i = 0; i < someUser.length; i++) {
+    result += someUser[i].name + ', ' + someUser[i].balance + '\n';
+  }
+ 
+  return (result.slice(0, result.length - 1));
 }
