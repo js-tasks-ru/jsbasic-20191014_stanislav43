27e23e3bae4b544d51e3534efe17b5e6c9db274f
BasankoSV <basankosv@gmail.com>
2020-01-22 18:46:05 +0500

    Написал функцию highlight, исправил ошибку

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -6,9 +6,9 @@ function highlight(table) {
   let colAge;
 
   for (let col = 0; col < colHead.length; col++) {
-    if (colHead[col].innerText == 'Status') { colStatus = col; }
-    if (colHead[col].innerText == 'Gender') { colGender = col; }
-    if (colHead[col].innerText == 'Age') { colAge = col; }
+    if (colHead[col].innerHTML == 'Status') { colStatus = col; }
+    if (colHead[col].innerHTML == 'Gender') { colGender = col; }
+    if (colHead[col].innerHTML == 'Age') { colAge = col; }
   }
 
   let tBody = table.querySelector('tbody');
@@ -18,19 +18,19 @@ function highlight(table) {
     let cellStatus = rows[i].cells[colStatus];
     if (cellStatus.hasAttribute('data-available')) { 
       if (cellStatus.dataset.available == 'true') { 
-        rows[i].classList.add('available') 
+        rows[i].classList.add('available'); 
       }
-        else if (cellStatus.dataset.available == 'false') {
-        rows[i].classList.add('unavailable') 
+      else if (cellStatus.dataset.available == 'false') {
+        rows[i].classList.add('unavailable'); 
       }
     } else { rows[i].setAttribute('hidden', 'hidden'); 
-  }  
+    }  
    
-    if (rows[i].cells[colGender] = 'm') { 
-      rows[i].classList.add('male')
-      } else if (rows[i].cells[colGender] = 'f') { 
-        rows[i].classList.add('female'); 
-      }
+    if (rows[i].cells[colGender].innerText == 'm') { 
+      rows[i].classList.add('male');
+    } else if (rows[i].cells[colGender].innerText == 'f') { 
+      rows[i].classList.add('female'); 
+    }
     
     if (+rows[i].cells[colAge].innerText < 18) { 
       rows[i].style = 'text-decoration: line-through';
