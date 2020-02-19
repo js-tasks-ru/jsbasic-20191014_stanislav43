51e0d203c2369dd6c17046dbdef3e54f332ae7a5
BasankoSV <basankosv@gmail.com>
2020-02-19 17:08:25 +0500

    Компонент Очищаемая табилца (ClearedTable)

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,29 +1,47 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
-     *          name: 'Ilia',
-     *          age: 25,
-     *          salary: '1000',
-     *          city: 'Petrozavodsk'
-     *      },
- *
- * @constructor
- */
 class ClearedTable {
   constructor(data) {
-    this.el = document.createElement('table');
+    this.el = document.createElement("table");
     this.data = data;
-  }
+    this.el.classList.toggle("pure-table");
+    let tHead = document.createElement("thead");
+    let tBody = document.createElement("tbody");
+    tHead.innerHTML = `
+                    <tr>
+                      <td>Name</td>
+                      <td>Age</td>
+                      <td>Salary</td>
+                      <td>City</td>
+                      <td></td>
+                    </tr>
+  `;
+    this.el.append(tHead);
+    tBody.innerHTML = data
+      .map(
+        user => `
+                    <tr id="${user.id}">
+                      <td>${user.name}</td>
+                      <td>${user.age}</td>
+                      <td>${user.salary}</td>
+                      <td>${user.city}</td>
+                      <td><a href="#delete">X</a></td>
+                    </tr>
+                  `
+      )
+      .join("");
+    this.el.append(tBody);
+    this.el.addEventListener("click", event => this.onClick(event));
+  } //к конструктору
 
-  /**
-   * Метод который выщывается после удалении строки
-   * @param {number} id - идентификатор удаляемого пользователя
-   */
-  onRemoved(id) {}
-}
+  onClick(event) {
+    if (event.target.getAttribute("href") == "#delete") {
+      let userId = event.target.closest("tr").getAttribute("id");
+      event.target.closest("tr").remove();
+      this.onRemoved(+userId);
+    }
+  }
+  onRemoved(userId) {
+    console.log(`Из таблицы удален пользователь ${userId}`);
+  }
+} // к классу
 
 window.ClearedTable = ClearedTable;
